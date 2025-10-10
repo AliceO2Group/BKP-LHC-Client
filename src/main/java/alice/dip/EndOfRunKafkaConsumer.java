d93d1f9ec61dc9cd2d042553e9d9ae33eabbde2d
George Raduta <george.raduta@cern.ch>
2025-10-10 11:45:03 +0200

    Migrate project structure to maven standard

--- src/main/java/alice/dip/EndOfRunKafkaConsumer.java
+++ src/main/java/alice/dip/EndOfRunKafkaConsumer.java
@@ -13,7 +13,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 
-import alice.dip.AlicePB.NewStateNotification;
+import main.java.alice.dip.AlicePB.NewStateNotification;
 
 import java.time.Duration;
 import java.util.Arrays;
